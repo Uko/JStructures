6dbf20653d5df2d0521abf0bd30cdfe32caae1a8
Uko <ukodrag@gmail.com>
2010-12-08 17:41:31 +0200

    Added unfinished RBliver (a reduced Red-Black tree with only a key value). Dijkstra Algorithm has BUGS!!!

--- test/Graph/DijkstraAlgorithmTest.java
+++ test/Graph/DijkstraAlgorithmTest.java
@@ -4,8 +4,6 @@
  */
 package Graph;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -39,5 +37,30 @@ public class DijkstraAlgorithmTest
 			System.out.println(i);
 		}
 		assertEquals(20.0, helper.getLength(), 0.0);
+	}
+	/**
+	 * Test of solve method, of class DijkstraAlgorithm.
+	 */
+	@Test
+	public void testSolve2()
+	{
+		Graph instance = new Graph(6);
+		instance.addConnection(2, 0, 1);
+		instance.addConnection(4, 0, 2);
+		instance.addConnection(7, 0, 3);
+		instance.addConnection(5, 0, 5);
+		instance.addConnection(6, 1, 3);
+		instance.addConnection(8, 1, 4);
+		instance.addConnection(6, 2, 5);
+		instance.addConnection(6, 3, 4);
+		instance.addConnection(1, 3, 5);
+		instance.addConnection(6, 4, 5);
+		DijkstraAlgorithm helper = new DijkstraAlgorithm(instance, 2, 1);
+		System.out.println();
+		for(int i:helper.getPath())
+		{
+			System.out.println(i);
+		}
+		assertEquals(6.0, helper.getLength(), 0.0);
 	}
 }
